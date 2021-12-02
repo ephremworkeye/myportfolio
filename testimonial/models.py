50d88d7aa24fd510659da364eff6af97c62c061a
Ephrem W <ephremworkeye@gmail.com>
2021-12-01 20:09:00 -0800

    models Testimonial updated

--- testimonial/models.py
+++ testimonial/models.py
@@ -10,3 +10,8 @@ class Testimonial(models.Model):
     quote = models.TextField()
     role = models.CharField(max_length=250)
     image = models.ImageField(upload_to='testimonial', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name
