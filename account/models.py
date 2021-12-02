a386a319f82e599454a084927ba077dc57db17d6
Ephrem W <ephremworkeye@gmail.com>
2021-12-01 20:24:57 -0800

    Profile models updated

--- account/models.py
+++ account/models.py
@@ -11,3 +11,11 @@ class Profile(models.Model):
         Skill, on_delete=models.CASCADE, related_name='user_skills')
     linkden_url = models.URLField(max_length=250, blank=True)
     website_url = models.URLField(max_length=250, blank=True, null=True)
+    image = models.ImageField(upload_to='profile', blank=True, null=True)
+    title = models.CharField(max_length=250)
+    resume = models.FileField(upload_to='resume', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name
