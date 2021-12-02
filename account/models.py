ce9ea1be9452ff14cc7d0bb9ef74990d9bc45e71
Ephrem W <ephremworkeye@gmail.com>
2021-12-01 18:16:30 -0800

    account app added and configured, prfile models added

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from skill.models import Skill
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    skill = models.ForeignKey(
+        Skill, on_delete=models.CASCADE, related_name='user_skills')
+    linkden_url = models.URLField(max_length=250, blank=True)
+    website_url = models.URLField(max_length=250, blank=True, null=True)
