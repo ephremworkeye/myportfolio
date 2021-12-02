28562d508f93eb5df6fbb2d3a7dab3a0dc331c76
Ephrem W <ephremworkeye@gmail.com>
2021-12-01 18:25:26 -0800

    portfolio models added configured, portfolio models added

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -40,6 +40,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'portfolio',
+    'skill',
+    'testimonial'
+    'account'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from skill.models import Skill
 
 # Create your models here.
+
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
+    image = models.ImageField(upload_to='portfolios/', blank=True)
+    is_published = models.BooleanField(default=True, null=True)
+    description = models.TextField(blank=True)
+    website_link = models.URLField(max_length=250, blank=True)
+    github_link = models.URLField(max_length=350, blank=True)
+    skill = models.ManyToManyField(
+        'Skill', through='PortfolioSkill', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
