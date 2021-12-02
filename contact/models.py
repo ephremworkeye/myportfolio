0c3b5a7222372ba3607b26b6979a9cb44fa02346
Ephrem W <ephremworkeye@gmail.com>
2021-12-01 19:46:07 -0800

    Contact model added

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f"{first_name} {last_name}"
