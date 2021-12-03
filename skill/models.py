55a1bbb3ee3cd64a80ad717ca4e33d4643fc0d0b
Ephrem W <ephremworkeye@gmail.com>
2021-12-03 02:16:45 -0800

    __str__ method added

--- skill/models.py
+++ skill/models.py
@@ -20,3 +20,6 @@ class Skill(models.Model):
 class Library(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
