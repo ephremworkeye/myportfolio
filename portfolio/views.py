aad676d32e79040da0bac0be80940c956e50db12
Ephrem W <ephremworkeye@gmail.com>
2021-12-04 03:36:18 -0800

    added about and testimonial in main page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Portfolio
+from skill.models import Skill
+from testimonial.models import Testimonial
 
 # Create your views here.
 
 
 def portfolio_list(request):
     portfolios = Portfolio.objects.filter(is_published=True)
-    return render(request, 'portfolio/portfolio_list.html', {'portfolios': portfolios})
+    skills = Skill.objects.filter(is_main_skill=True)
+    testimonials = Testimonial.objects.filter(is_active=True)[:3]
+    return render(request, 'portfolio/portfolio_list.html', {'portfolios': portfolios, 'skills': skills, 'testimonials': testimonials})
 
 
 def portfolio_detail(request, id, slug):
--- testimonial/admin.py
+++ testimonial/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Testimonial
 
 # Register your models here.
+
+
+@admin.register(Testimonial)
+class AdminTestimonial(admin.ModelAdmin):
+    list_display = ['user', 'quote', 'role', 'is_active']
