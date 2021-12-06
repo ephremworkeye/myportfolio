670e5640097dfcfbdb1155ce95e188a9e8dbb99a
Ephrem W <ephremworkeye@gmail.com>
2021-12-06 01:19:33 -0800

    contact functionality added

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail, get_connection
+from .forms import ContactForm
+
 
 # Create your views here.
+def contact(request):
+    sent = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            form.save()
+            send_mail(
+                cd['first_name'], cd['message'], cd.get(
+                    'email', 'noreply@example.com'), ['ephremwube@gmail.com'])
+            return HttpResponseRedirect('/contact?sent=True')
+    else:
+        form = ContactForm()
+        if 'sent' in request.GET:
+            sent = True
+    return render(request, 'contact/contact.html', {'form': form, 'sent': sent})
