7e61d30df9347c504b927d3534f6fc14163e501d
Ephrem W <ephremworkeye@gmail.com>
2021-12-06 19:05:43 -0800

    contact model email field changed and template adjustment

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Contact(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150)
     message = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
