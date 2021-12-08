fdcdc134034d81a26f8c2f2d493b979c00966fc7
Ephrem W <ephremworkeye@gmail.com>
2021-12-08 02:32:01 -0800

    navbar menu active usign js and css

--- portfolio/static/scripts.js
+++ portfolio/static/scripts.js
@@ -1,5 +1,18 @@
 // alert('hello world');
 
+
+var header = document.getElementById('nav-menu');
+var navLinks = header.getElementsByClassName('nav-link');
+console.log(navLinks)
+// Loop through the buttons and add the active class to the current / clicked button
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
 var typed = new Typed(".auto-input", {
     strings: ["Ephrem Wube", "Full Stack Developer", "Django Developer"],
     typeSpeed: 100,
