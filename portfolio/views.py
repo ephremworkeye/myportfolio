a7ea9b948605abaf45de1a22e07202b13d1094fd
Ephrem W <ephremworkeye@gmail.com>
2021-12-07 16:39:31 -0800

    added pagination functionality and include templates

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Portfolio
 from skill.models import Skill
 from account.models import Profile
@@ -10,7 +11,17 @@ from contact.forms import ContactForm
 
 
 def portfolio_list(request):
-    portfolios = Portfolio.objects.filter(is_published=True)
+    # portfolios = Portfolio.objects.filter(is_published=True)
+
+    object_list = Portfolio.objects.filter(is_published=True)
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        portfolios = paginator.page(page)
+    except PageNotAnInteger:
+        portfolios = paginator.page(1)
+    except EmptyPage:
+        portfolios = paginator.page(paginator.num_pages)
     skills = Skill.objects.filter(is_main_skill=True)
     testimonials = Testimonial.objects.filter(is_active=True)[:3]
     profiles = Profile.objects.filter(user=request.user.is_superuser)
