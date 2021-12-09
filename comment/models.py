ee250ff55dbc827c07661b51fd3fcbbd6c50fa21
Ephrem W <ephremworkeye@gmail.com>
2021-12-09 01:00:06 -0800

    comment model, form added and comment model registered

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from portfolio.models import Portfolio
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    message = models.TextField()
+    portfolio = models.ForeignKey(
+        Portfolio, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.portfolio}"
