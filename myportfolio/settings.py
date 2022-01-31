6a995753b7556ce059411d678a8d392452b11cca
Ephrem W <ephremworkeye@gmail.com>
2022-01-31 12:32:52 -0800

    added domain name

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
 
-ALLOWED_HOSTS = ["myephremprofile.herokuapp.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["myephremprofile.herokuapp.com", "127.0.0.1",
+                 "localhost", 'www.myephrem.com', 'myephrem.com']
 
 
 # Application definition
