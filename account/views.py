986c5c631ec223a45d6076ea24b600707d45bd1c
Ephrem W <ephremworkeye@gmail.com>
2022-02-01 17:27:28 -0800

    test

--- account/views.py
+++ account/views.py
@@ -10,7 +10,8 @@ def download_resume(request, path):
     file_path = os.path.join(settings.MEDIA_ROOT, path)
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type='resume')
+            response = HttpResponse(
+                fh.read(), content_type='application/resume')
             response['Content-Disposition'] = 'inline;filename=' + \
                 os.path.basename(file_path)
             return response
